21cfe67fd2e564328f2b36ad4bc8fbe62314df2d
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-06-08 16:16:06 +0300

    added vuex

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,70 @@
 <template>
   <v-app>
+    
+    <v-navigation-drawer
+      app
+      left
+      absolute
+      temporary
+      v-model="drawer"
+    >
+      
+      <v-list-item-group>
+        
+        <v-list-item v-for="item in menu_items" :key="item.id">
+          <v-list-item-title>
+            <router-link :to="item.url">{{ item.name }}</router-link>
+          </v-list-item-title>
+        </v-list-item>
+        
+      </v-list-item-group>
+      
+    </v-navigation-drawer>
+    
     <v-app-bar
       app
       color="red"
     >
-
-    <v-app-bar-title>
-      <h3 class="text-h4 white--text">iFood</h3>
-    </v-app-bar-title>
-
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title>
+        <h3 class="text-h4 white--text">iFood</h3>
+      </v-app-bar-title>
     </v-app-bar>
-
+    
     <v-main>
       <router-view></router-view>
     </v-main>
-     
-   </v-app>
+    
+  </v-app>
 </template>
+
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data: () => ({
+    drawer: false,
+    menu_items: [
+      {
+        id: 0,
+        name: 'Список заведения',
+        url: '/'
+      },
+      {
+        id: 1,
+        name: 'О нас',
+        url: '#'
+      },
+      {
+        id: 2,
+        name: 'Контакты',
+        url: '#'
+      },
+      {
+        id: 3,
+        name: 'Помощь',
+        url: '#'
+      }
+    ]
+  })
 }
 </script>
