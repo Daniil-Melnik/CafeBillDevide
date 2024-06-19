88a890c824ffb4e6506cefa28a2ae9232d6a0603
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-06-19 20:59:15 +0300

    29 -- Added function for summary money

--- src/components/ListRelat.vue
+++ src/components/ListRelat.vue
@@ -4,9 +4,9 @@
         <v-table>
           <thead>
             <tr>
-              <th>Имя</th>
-              <th>Потратил</th>
-              <th></th>
+              <th>Кто</th>
+              <th>Кому</th>
+              <th>Сколько</th>
             </tr>
           </thead>
           <tbody v-for="(pC, index) in getPersWithReceipts()" :key="index">
@@ -17,6 +17,7 @@
             </tr>
           </tbody>
         </v-table>
+        <p>{{ getMoneyRelat() }}</p>
     </v-container>
   </template>
   <script>
@@ -33,6 +34,29 @@
     methods: {
      getPersWithReceipts(){
         return this.$store.getters.getPersWithRec
+     },
+
+     getMoneyRelat(){
+      var persAdr = this.getPersWithReceipts();
+      var allPers = this.$store.getters.PERSONS;
+      var persLen = 0;
+      var res = [];
+      for (var i in persAdr){
+        var currReceipt = this.$store.getters.getCheckByName(persAdr[i]);
+        var currProducts = currReceipt.products
+        for (var a in allPers){
+          var sum = 0;
+          for (var cP in currProducts){
+            if (currProducts[cP].eatPersons.indexOf(allPers[a].name, 0) != -1){
+              persLen = currProducts[cP].eatPersons.length
+              sum += currProducts[cP].price / persLen;
+            }
+          }
+          res.push({nameFrom : allPers[a].name, nameTo: persAdr[i], money: sum})
+        }
+      }
+      console.log(res);
+      return 1;
      }
     }
     
