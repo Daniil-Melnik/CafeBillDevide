78afca15d80a10933b9257e43d08a6813854832b
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-06-20 13:28:37 +0300

    30 -- Distribution lists have been added

--- src/components/ListRelat.vue
+++ src/components/ListRelat.vue
@@ -1,23 +1,33 @@
 <template>
     <v-container>
-        <h2>Кто кому что должен</h2>
-        <v-table>
-          <thead>
-            <tr>
-              <th>Кто</th>
-              <th>Кому</th>
-              <th>Сколько</th>
-            </tr>
-          </thead>
-          <tbody v-for="(pC, index) in getPersWithReceipts()" :key="index">
-            <tr v-for="p in 3" v-bind:key="p">
-              <td>{{ pC }}</td>
-              <td></td>
-              <td></td>
-            </tr>
-          </tbody>
-        </v-table>
-        <p>{{ getMoneyRelat() }}</p>
+        <h2>Распределение {{checkboxLbl }}</h2>
+        <v-radio-group v-model="radios" @update:modelValue="updLabels">
+          <v-radio label="Кто - кому" value = 1></v-radio>
+          <v-radio label="Кому - кто" value= 2 ></v-radio>
+        </v-radio-group>
+        <div v-if="radios != 0">
+          <v-select
+            :items="persons()"
+            v-model="persFromName"
+            @update:modelValue="null"
+            label= "Выберите человека"
+          ></v-select>
+          <v-table>
+            <thead>
+              <tr>
+                <th>{{ tableLbl }}</th>
+                <th>Сколько</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(pMR, index) in getMoneyRelat()" :key="index">
+                <td v-if="radios == 1">{{ pMR.nameTo }}</td>
+                <td v-if="radios == 2">{{ pMR.nameFrom }}</td>
+                <td>{{ pMR.money }}</td>
+              </tr>
+            </tbody>
+          </v-table>
+        </div>
     </v-container>
   </template>
   <script>
@@ -28,13 +38,36 @@
     },
     data() {
       return {
-        
+        persFromName: "",
+        radios: 0,
+        checkboxLbl: "",
+        tableLbl: ""
       }
     },
     methods: {
      getPersWithReceipts(){
         return this.$store.getters.getPersWithRec
      },
+
+     updLabels(){
+      if (this.radios == 1){
+        this.checkboxLbl = ": Кто должен"
+        this.tableLbl = "Кому"
+      }
+      if (this.radios == 2){
+        this.checkboxLbl = ": Кому должны"
+        this.tableLbl = "Кто"
+      }
+     },
+
+     persons(){
+        var arr = this.$store.getters.PERSONS
+        var eArr = [""]
+        for (var i = 0; i < arr.length; i++){
+          eArr.push(arr[i].name)
+        }
+        return eArr
+      },
 
      getMoneyRelat(){
       var persAdr = this.getPersWithReceipts();
@@ -52,11 +85,14 @@
               sum += currProducts[cP].price / persLen;
             }
           }
-          res.push({nameFrom : allPers[a].name, nameTo: persAdr[i], money: sum})
+          if (sum != 0 && allPers[a].name != persAdr[i]){
+            if (allPers[a].name == this.persFromName && this.radios == 1) res.push({nameFrom : allPers[a].name, nameTo: persAdr[i], money: Math.ceil(sum)})
+            if (persAdr[i] == this.persFromName && this.radios == 2) res.push({nameFrom : allPers[a].name, nameTo: persAdr[i], money: Math.ceil(sum)})
+          }
         }
       }
       console.log(res);
-      return 1;
+      return res
      }
     }
     
