8f1b21077d7a0d3d4b7eb7cbeceda18fddf5e900
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-06-24 16:47:23 +0300

    55 -- Added RelatTable-component

--- src/components/ListRelat.vue
+++ src/components/ListRelat.vue
@@ -13,26 +13,17 @@
             @update:modelValue="persFromObj = this.$store.getters.getPersonByName(persFromName)"
             label= "Выберите человека"
           ></v-select>
-          <!-- выделить в компонент -->
-          <v-table>
-            <thead>
-              <tr>
-                <th>{{ tableLbl }}</th>
-                <th>Сколько</th>
-              </tr>
-            </thead>
-            <tbody v-if="persFromObj != null">
-              <tr v-for="(pMR, index) in getCurrMoneyRelat()" :key="index">
-                <td v-if="radios == 1">{{  getPersNameById(pMR.nameTo) }}</td>
-                <td v-if="radios == 2">{{ getPersNameById(pMR.nameFrom) }}</td>
-                <td>{{ pMR.money }}</td>
-              </tr>
-            </tbody>
-          </v-table>
+          <RelatTable
+            :persFromObj= "persFromObj"
+            :radios= "radios"
+            :tableLbl= "tableLbl"
+          >
+          </RelatTable>
         </div>
     </v-container>
   </template>
   <script>
+  import RelatTable from './RelatTable.vue'
   export default {
     name: 'ListRelat',
     computed: {
@@ -48,10 +39,6 @@
       }
     },
     methods: {
-     getPersWithReceipts(){
-        return this.$store.getters.getPersWithRec
-     },
-
      updLabels(){
       if (this.radios == 1){
         this.checkboxLbl = ": Кто должен"
@@ -72,84 +59,9 @@
         return eArr
       },
 
-      getPersNameById(id){
-        var person = this.$store.getters.getPersonById(id);
-        return person.name
-      },
-
-     getNRepMoneyRelat(){
-      var persAdr = this.getPersWithReceipts();
-      var allPers = this.$store.getters.PERSONS;
-      var persLen = 0;
-      var res = [];
-      for (var i in persAdr){
-        var currReceipt = this.$store.getters.getCheckByName(persAdr[i].id);
-        var currProducts = currReceipt.products
-        for (var a in allPers){
-          var sum = 0;
-          for (var cP in currProducts){
-            if (currProducts[cP].eatPersons.indexOf(allPers[a].id, 0) != -1){
-              persLen = currProducts[cP].eatPersons.length
-              sum += currProducts[cP].price / persLen;
-            }
-          }
-          if (sum != 0 && allPers[a].name != persAdr[i].name){
-            res.push({nameFrom : allPers[a].id, nameTo: persAdr[i].id, money: Math.ceil(sum)})
-          }
-        }
-      }
-      console.log(res)
-      res = this.checkRepets(res) // Очистка от зеркальных пар
-      console.log(res)
-      return res
-     },
-
-
-     getCurrMoneyRelat(){
-      var objRelat = this.getNRepMoneyRelat();
-      var rO = null;
-      var res = []
-      for (var i in objRelat){
-        rO = objRelat[i]
-        if (rO.nameFrom == this.persFromObj.id && this.radios == 1) res.push(rO)
-        if (rO.nameTo == this.persFromObj.id && this.radios == 2) res.push(rO)
-      }
-      return res
     },
-
-    removeFromArr(arr, o){
-      return arr.filter(el => !(el.nameFrom === o.nameFrom && el.nameTo === o.nameTo && el.money === o.money));
-    },
-
-    checkRepets(objRelat) {
-      let resObjRelat = objRelat.slice();  // Создаем копию массива, чтобы не изменять оригинал
-      let addList = [];
-      let o1 = null;
-      let o2 = null;
-      let o2L = null;
-
-      for (let oR of objRelat) {
-          o1 = oR;
-          o2L = objRelat.filter((o) => o.nameFrom === o1.nameTo && o.nameTo === o1.nameFrom);
-          o2 = o2L[0];
-
-          if (o2 != null) {
-              let cond1 = resObjRelat.filter((rO) => rO.nameFrom === o1.nameFrom && rO.nameTo === o1.nameTo);
-              let cond2 = resObjRelat.filter((rO) => rO.nameFrom === o1.nameTo && rO.nameTo === o1.nameFrom);
-
-              if (cond1.length !== 0 || cond2.length !== 0) {
-                  if (o2.money > o1.money) addList.push({ nameFrom: o2.nameFrom, nameTo: o2.nameTo, money: o2.money - o1.money });
-                  if (o1.money > o2.money) addList.push({ nameFrom: o1.nameFrom, nameTo: o1.nameTo, money: o1.money - o2.money });
-
-                  console.log("****");
-                  resObjRelat = this.removeFromArr(resObjRelat, o1);
-                  resObjRelat = this.removeFromArr(resObjRelat, o2);
-              }
-          }
-      }
-      resObjRelat = resObjRelat.concat(addList);
-      return resObjRelat;
-    }
+    components: {
+      RelatTable
     }
   };
   </script>
