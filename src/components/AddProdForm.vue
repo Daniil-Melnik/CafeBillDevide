00017aef58f5e123eee47cbd53c418f534a02649
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-06-29 10:33:39 +0300

    68 -- added alert-window for addProdForm

--- src/components/AddProdForm.vue
+++ src/components/AddProdForm.vue
@@ -6,6 +6,7 @@
         label="Новый продукт"
         hint="Любая последовательность букв и цифр"
         :rules="is_valid_title"
+        @update:modelValue="alert = false"
       ></v-text-field>
 
       <v-number-input
@@ -16,13 +17,26 @@
         hint="Максимум 50000 рублей"
         :hideInput="false"
         :inset="false"
-        :rules="not_null"
+        :rules="is_valid_price"
         min="0"
         max="50000"
         v-model="newProdPrice"
+        @update:modelValue="alert = false"
       ></v-number-input>
       <v-btn class="mt-2" @click="sendPack()" block>Добавить</v-btn>
     </v-form>
+    <v-alert
+      v-model="alert"
+      border="start"
+      close-label="Закрыть"
+      color="#BC13FE"
+      title="Продукт некорректен"
+      variant="tonal"
+      class="margin-top-15"
+      closable
+    >
+    Проверте {{ !isValidTitle() ? "наименование" : "" }} {{ (!isValidPrice() && !isValidTitle()) ? "и" : "" }} {{ !isValidPrice() ? "цену" : "" }}
+    </v-alert>
   </div>
 </template>
 
@@ -34,11 +48,14 @@ export default {
     return {
       newProdTitle: "",
       newProdPrice: 0,
-      not_null: [
+      alert: false,
+
+      is_valid_price: [
         value => {
           if (value == null) return 'Не может быть пустым'
         },
       ],
+
       is_valid_title: [
         value => {
           var re = /^[\u0400-\u04FF0-9A-Za-z]+$/
@@ -50,8 +67,21 @@ export default {
     };
   },
   methods: {
+    isValidTitle(){
+      var re = /^[\u0400-\u04FF0-9A-Za-z]+$/
+      return (re.test(this.newProdTitle))
+    },
+
+    isValidPrice(){
+      return (this.newProdPrice != null)
+    },
+
     sendPack() {
-      this.$emit('senddata', this.newProdPrice, this.newProdTitle);
+      if (this.isValidTitle() && this.isValidPrice()){
+        this.$emit('senddata', this.newProdPrice, this.newProdTitle);
+        this.alert = false
+      }
+      else this.alert = true
     }
   }
 };
